1e9e71fe88fd78307f1ece2be689b46173082077
曹一梁 <caoyiliang@me.com>
2021-05-25 09:04:10 +0800

    添加项目文件。

---
+++ Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "*";
+            factory.UserName = "*";
+            factory.Password = "*";
+            _ = Task.Run(async () =>
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare("test_q", true, false, false, null);
+
+                        channel.ExchangeDeclare("testDirect", "direct", true);
+
+                        channel.QueueBind(queue: "test_q",
+                              exchange: "testDirect",
+                              routingKey: "test_q");
+
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) =>
+                       {
+                           var message = ea.Body.ToArray();
+                       };
+                        channel.BasicConsume("test_q", true, consumer);
+
+                        while (true)
+                        {
+                            await Task.Delay(10);
+                        }
+                    }
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "*";
+            factory.UserName = "*";
+            factory.Password = "*";
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("test_q", true, false, false, null);
+            channel.ExchangeDeclare("testDirect", "direct", true);
+            channel.QueueBind(queue: "test_q",
+                      exchange: "testDirect",
+                      routingKey: "test_q");
+
+            int i = 0;
+            while (true)
+            {
+                channel.BasicPublish("testDirect", "test_q", null, new byte[] { (byte)i++ });
+                await Task.Delay(10);
+            }
+        }
+    }
+}
